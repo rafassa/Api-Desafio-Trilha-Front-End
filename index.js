60be76bb671bc37d594bd99a56959f939e224d63
rafassa <rafassa95@gmail.com>
2025-05-20 02:11:23 -0300

    imports errados

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { fileURLToPath } from "url";
-import { dirname, join, resolve } from "path";
+import { dirname, } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
